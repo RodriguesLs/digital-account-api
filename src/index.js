5e22924191dd02d26584ed08e438504b6583f56b
Alysonn de Araujo <alysonn2n@gmail.com>
2021-11-23 10:04:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+/* teste padrão */
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+/* routes  */
+require('./controller/authController')(app);
+
+app.listen(3000, () => {
+    console.log('Welcome to Json reader API')
+})
