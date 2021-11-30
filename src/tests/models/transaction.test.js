b3f8afc4f8cb24c841adfec55cbe29455b797177
Rodrigues <tercmt-feliperc@ftd.com.br>
2021-11-30 14:04:45 -0300

    test(model): fix model tests

--- src/tests/models/transaction.test.js
+++ src/tests/models/transaction.test.js
@@ -18,33 +18,32 @@ describe('Transaction model test', () => {
   it('Should register a transaction', () => {
     expect(listTransactionHistoryService.showQuantity()).toEqual(0);
 
-    let response = transaction.create(new_transaction);
+    let response = transaction.create(JSON.stringify(new_transaction));
 
     expect(response['sender-document']).toEqual(new_transaction['sender-document']);
-    expect(response['available-limit']).toEqual(200);
   });
 
   it('Should register sender-transaction correctly', () => {
+    transaction.createHistory(sender_account.document, new_transaction);
     let response = transaction.history(sender_account.document);
 
     expect(response).toEqual([{
       "sender-document": "111.111.111-11",
       "receiver-document": "999.999.999-99",
-      value: -100,
-      datetime: expect.anything(),
-      "available-limit": 200
+      value: 100,
+      datetime: expect.anything()
     }]);
   });
 
   it('Should register receiver-transaction correctly', () => {
+    transaction.createHistory(receiver_account.document, new_transaction);
     let response = transaction.history(receiver_account.document);
 
     expect(response).toEqual([{
       "receiver-document": "999.999.999-99",
       "sender-document": "111.111.111-11",
       value: 100,
-      datetime: expect.anything(),
-      "available-limit": 400
+      datetime: expect.anything()
     }]);
   });
 });
